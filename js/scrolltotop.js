db16b464e83abe137490ed172bbbfe24de42813f
Vladislav <nafanya102@gmail.com>
2025-02-03 17:06:26 +0200

    df

--- js/scrolltotop.js
+++ js/scrolltotop.js
@@ -16,3 +16,36 @@ function scrollToTop() {
         behavior: "smooth"
     });
 }
+
+
+// // Завантаження музики
+// const audio = new Audio('./McDonalds Rich.mp3');
+
+// // Відображення модального вікна при завантаженні
+// window.onload = () => {
+//     const modal = document.getElementById('modal');
+//     const enterButton = document.getElementById('enter-button');
+
+//     modal.style.display = 'flex';
+
+//     enterButton.addEventListener('click', () => {
+//         modal.style.display = 'none';
+//         audio.play();
+//     });
+// };
+
+// // Кнопка для вмикання/вимикання музики
+// const musicButton = document.getElementById('music-button');
+// let isPlaying = false;
+
+// musicButton.addEventListener('click', () => {
+//     if (audio.paused) {
+//         audio.play();
+//         isPlaying = true;
+//         musicButton.innerHTML = "🎵"; // Змінити іконку, якщо потрібно
+//     } else {
+//         audio.pause();
+//         isPlaying = false;
+//         musicButton.innerHTML = "🔇"; // Змінити іконку, якщо потрібно
+//     }
+// });
